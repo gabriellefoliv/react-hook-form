9df5c00a0e3c9049807aaff15d33c2374a708792
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-08-17 17:42:21 -0300

    feat: field arrays

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 // [] Upload de arquivos
 // [] Composition Pattern
 
+Field Array:
+Formulários que é possível incluir mais informações de um campo.
+Ex: Cadastro de um módulo de aulas.
+Cada módulo tinha várias aulas, só que não um número certo.
+Podia ser 5 aulas, 10 aulas, 15 aulas...
+O usuário consegue adicionar quantas aulas quiser (adicionar ou remover mais desses campos)
+
 # React + TypeScript + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./styles/global.css";
 
 import { useState } from "react";
-import { useForm } from 'react-hook-form'
+import { useForm, useFieldArray } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 
@@ -16,23 +16,33 @@ const createUserFormSchema = z.object({
   email: z.string()
     .min(1, "O e-mail é obrigatório")
     .email("Formato de email inválido")
-    .toLowerCase()
-    .refine(email => {
-      return email.endsWith('@rocketseat.com.br')
-    }, 'O e-mail precisa ser da Rocketseat'),
+    .toLowerCase(),
   password: z.string()
     .min(6, "A senha precisa de no mínimo 6 caracteres"),
+  techs: z.array(z.object({
+    title: z.string().min(1, "O título é obrigatório"),
+    knowledge: z.coerce.number().min(1).max(100),
+  })).min(2, 'Insira pelo menos duas tecnologias.')
 })
 
 type CreateUserFormData = z.infer<typeof createUserFormSchema>
 
 export function App() {
   const [output, setOutput] = useState('')
-  const { register, handleSubmit, formState: { errors } } = useForm<CreateUserFormData>({
+  const { register, handleSubmit, formState: { errors }, control } = useForm<CreateUserFormData>({
     resolver: zodResolver(createUserFormSchema),
   })
 
-  function createUser(data: unknown) {
+  const { fields, append } = useFieldArray({
+    control,
+    name: 'techs',
+  })
+
+  function addNewTech() {
+    append({ title: '', knowledge: 0 })
+  }
+
+  function createUser(data: CreateUserFormData) {
     setOutput(JSON.stringify(data, null, 2))
   }
 
@@ -48,7 +58,7 @@ export function App() {
             className="border border-zinc-200 shadow-sm rounded h-10 px-3"
             {...register('name')}
           />
-          {errors.name && <span>{errors.name.message}</span>}
+          {errors.name && <span className="text-red-500 text-sm">{errors.name.message}</span>}
         </div>
         
         <div className="flex flex-col gap-1">
@@ -58,7 +68,7 @@ export function App() {
             className="border border-zinc-200 shadow-sm rounded h-10 px-3"
             {...register('email')}
           />
-          {errors.email && <span>{errors.email.message}</span>}
+          {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
         </div>
 
         <div className="flex flex-col gap-1">
@@ -68,8 +78,39 @@ export function App() {
             className="border border-zinc-200 shadow-sm rounded h-10 px-3"
             {...register('password')}
           />
-          {errors.password && <span>{errors.password.message}</span>}
+          {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
         </div>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="" className="flex items-center justify-between">
+            Tecnologias
+            <button type="button" onClick={addNewTech} className="text-emerald-500 text-sm">Adicionar</button>
+          </label>
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className="flex gap-2">
+                  <div className="flex flex-1 flex-col gap-1">
+                    <input 
+                      type="text"  
+                      className="border border-zinc-200 shadow-sm rounded h-10 px-3"
+                      {...register(`techs.${index}.title`)}
+                      />
+                    {errors.techs?.[index]?.title && <span className="text-red-500 text-sm">{errors.techs?.[index]?.title?.message}</span>}
+                  </div>
+                  <div className="flex flex-col gap-1">
+                    <input 
+                      type="number"  
+                      className="w-16 border border-zinc-200 shadow-sm rounded h-10 px-3"
+                      {...register(`techs.${index}.knowledge`)}
+                      />
+
+                    {errors.techs?.[index]?.knowledge && <span className="text-red-500 text-sm">{errors.techs?.[index]?.knowledge?.message}</span>}      
+                  </div>
+                </div>
+              )
+            })}
+        </div>
+        {errors.techs && <span className="text-red-500 text-sm">{errors.techs.message}</span>}
 
         <button 
           type="submit"
